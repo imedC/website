47e5a5488932f4f4cd104db0f94343f6c0f4ff02
imed <imed08@hotmail.fr>
2017-04-09 11:26:18 +0100

    Initial commit

--- website/settings.py
+++ website/settings.py
@@ -83,7 +83,19 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
